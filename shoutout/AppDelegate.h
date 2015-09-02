dfc39346a6bea2e7b0ff9bd8f810c5c37422412b
Mayank Jain <mjmayank@gmail.com>
2015-09-02 12:47:38 -0500

    Initial Commit

---
+++ shoutout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shoutout
+//
+//  Created by Mayank Jain on 9/2/15.
+//  Copyright (c) 2015 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shoutout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  shoutout
+//
+//  Created by Mayank Jain on 9/2/15.
+//  Copyright (c) 2015 Mayank Jain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

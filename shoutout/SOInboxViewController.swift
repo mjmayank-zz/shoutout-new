22566de41059fff17463dca4b7cf8ee94ea9375f
Mayank Jain <mjmayank@gmail.com>
2015-09-22 11:49:26 -0500

    added messaging

--- shoutout/SOInboxViewController.swift
+++ shoutout/SOInboxViewController.swift
@@ -9,10 +9,54 @@
 import Foundation
 import UIKit
 
-class SOInboxViewController : UIViewController{
+class SOInboxViewController : UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet var tableView: UITableView!
+    var messages: [PFObject]?
+    
     override func viewDidLoad(){
         super.viewDidLoad();
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
+        
+        getMessages();
     }
     
+    func getMessages(){
+        let query = PFQuery(className: "Messages");
+        query.whereKey("to", equalTo: PFUser.currentUser()!);
+        query.findObjectsInBackgroundWithBlock { (results:[PFObject]?, error:NSError?) -> Void in
+            self.messages = results;
+            self.tableView.reloadData();
+        }
+    }
     
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int{
+            if let messages = messages{
+                return messages.count;
+            }
+            return 0;
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+            let cell = tableView.dequeueReusableCellWithIdentifier("messageCell", forIndexPath: indexPath) as! messagesCell;
+            if let messages = messages{
+                let message = messages[indexPath.row].objectForKey("message") as? String;
+                cell.bodyLabel.text = message;
+            }
+            return cell;
+    }
+    
+    @IBAction func didPressDoneButton(sender: AnyObject) {
+        self .dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+}
+
+class messagesCell : UITableViewCell{
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var bodyLabel: UILabel!
+    var object : PFObject!
 }

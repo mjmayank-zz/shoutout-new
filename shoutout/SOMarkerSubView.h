ce170cd26df89ce6f32ffb17a04547621eedb2fc
Mayank Jain <mjmayank@gmail.com>
2015-10-03 13:16:31 -0500

    Added marker subview

---
+++ shoutout/SOMarkerSubView.h
@@ -0,0 +1,13 @@
+//
+//  SOMarkerSubView.h
+//  shoutout
+//
+//  Created by Mayank Jain on 10/3/15.
+//  Copyright © 2015 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOMarkerSubView : UIView
+
+@end
---
+++ shoutout/SOMarkerSubView.m
@@ -0,0 +1,21 @@
+//
+//  SOMarkerSubView.m
+//  shoutout
+//
+//  Created by Mayank Jain on 10/3/15.
+//  Copyright © 2015 Mayank Jain. All rights reserved.
+//
+
+#import "SOMarkerSubView.h"
+
+@implementation SOMarkerSubView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
